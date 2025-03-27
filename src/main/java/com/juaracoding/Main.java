7b45525e630892951b082d59c630cf21f2efa6c2
Michael Laksa Kharisma <mike_charism@outlook.com>
2025-03-27 19:14:09 +0700

    First commit

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,15 @@
+package com.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        if (true) {
+            System.out.println("Statement IF");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
